f3c5f09041e525441ed03d95feaa476613248b2f
KrisjanisElksnitis <krisjaniselksnitis@gmail.com>
2021-12-02 23:30:47 +0200

    commiting changes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,8 @@
-var burger = document.getElementById("burger");
+const burger = document.getElementById("burger");
 const close_burger = document.getElementById("close_burger");
 const menu = document.getElementById("menu");
+
+var container_top = document.getElementById("container_top");
 
 const btnToTop = document.getElementById("btnToTop");
 
@@ -22,6 +24,18 @@ window.onscroll = function(e){
   }
   else if( window.scrollY != 0 && btnToTop.classList.contains("hidden")){
     btnToTop.classList.remove("hidden");
+  }
+
+  if((container_top.offsetHeight - 57) <= (window.pageYOffset + btnToTop.getBoundingClientRect().top)){
+    btnToTop.classList.remove("fixed");
+    btnToTop.classList.add("absolute");
+    btnToTop.style.top = container_top.offsetHeight;
+  }
+  else if ((window.scrollY + window.innerHeight) < (window.pageYOffset + btnToTop.getBoundingClientRect().top)){
+    console.log("NOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWW");
+    btnToTop.classList.add("fixed");
+    btnToTop.classList.remove("absolute");
+    btnToTop.style.top = null;
   }
 }
 
